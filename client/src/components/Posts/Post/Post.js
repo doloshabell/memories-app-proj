4dd90883155b37d3ef51da2f1ff40ef7e865317d
doloshabell <habelldol@gmail.com>
2022-04-10 13:26:56 +0700

    add func deletePost

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
-import useStyles from './styles'
+
+import useStyles from './styles';
+import { deletePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <Card className={classes.card}>
@@ -17,7 +21,7 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{color: 'white'}} size='small' onClick={() => setCurrentId(post._id)}>
+        <Button style={{ color: 'white' }} size='small' onClick={() => setCurrentId(post._id)}>
           <MoreHorizIcon fontSize='default' />
         </Button>
       </div>
@@ -28,12 +32,12 @@ const Post = ({ post, setCurrentId }) => {
         <Typography className={classes.title} variant='h5' gutterBottom>{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button size='small' color='primary' onClick={() => { }}>
           <ThumbUpAltIcon fontSize='small' />
           Like
           {post.likeCount}
         </Button>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize='small' />
           Delete
         </Button>
