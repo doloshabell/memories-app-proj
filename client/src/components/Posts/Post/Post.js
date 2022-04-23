673e13033df2771e856bb4cb824b73f8e2a0c55f
doloshabell <habelldol@gmail.com>
2022-04-23 20:51:53 +0700

    add auth with google login

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 
 import useStyles from './styles';
-import { deletePost } from '../../../actions/posts'
+import { deletePost, likePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
@@ -28,22 +28,16 @@ const Post = ({ post, setCurrentId }) => {
       <div className={classes.details}>
         <Typography variant='body2' color='textSecondary'>{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
+      <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
       <CardContent>
-        <Typography className={classes.title} variant='h5' gutterBottom>{post.message}</Typography>
+        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size='small' color='primary' onClick={() => { }}>
-          <ThumbUpAltIcon fontSize='small' />
-          Like
-          {post.likeCount}
-        </Button>
-        <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
-          <DeleteIcon fontSize='small' />
-          Delete
-        </Button>
+        <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize='small' />&nbsp; Like {post.likeCount}</Button>
+        <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize='small' />Delete</Button>
       </CardActions>
     </Card>
-  )
-}
+  );
+};
 
 export default Post
